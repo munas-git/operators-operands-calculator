5bd07cfcc3f0516f13ecc572848d9d67533bac0e
Ebereonwu Einstein Munachiso <einsteinmunachiso@gmail.com>
2022-08-10 16:24:44 +0100

    Added volumn and lenght function, edited html and server to accomodate new features

--- app.py
+++ app.py
@@ -11,11 +11,16 @@ def home():
         return render_template('home.html')
 
     elif request.method == 'POST':
+
         original_code = request.form.get('code_input')
         clean_code = original_code.replace('\n', ' ').replace('\t', ' ')
-        num_operators = operators_count(clean_code)
-        num_operands = operands_count(clean_code)
-        return render_template('result.html', num_operators = num_operators[2], num_operands = num_operands, original_code = original_code, clean_code = clean_code)
+        operators = operators_count(clean_code)
+        operands = operands_count(clean_code)
+        unique_operands_amount = operands[1]
+        unique_operators_amount = operators[3]
+        length_volumn = code_length_column(unique_operators_amount, unique_operands_amount) 
+
+        return render_template('result.html', num_operators = operators[2], num_operands = operands[0], original_code = original_code, clean_code = clean_code, length = length_volumn[0], volumn = length_volumn[1])
 
 
 
--- operands_operators.py
+++ operands_operators.py
@@ -1,3 +1,5 @@
+from math import *
+
 operators = [
         '+', '-', '*', '/', '%', '**', '//', '=', '+=', '-=', '*=', '/=', '%=', '//=', '**=', '&=', '|=', '^=',
         '>>=', '<<=','==', '!=','>', '<', '>=', '<=', 'and', 'or', 'not', 'is', 'is not', 'in', 'not in', '&',
@@ -12,10 +14,10 @@ def operators_count(code):
     This function calculates the number of operators and operands in a string of code (python)
 
     Parameter:
-    code: code string
+     code: code string
 
     return:
-    (operators_dict, operators_list, operators amount): Tuple containing operators and their occurence count, list containing operators and integer sum of operators.
+     (operators_dict, operators_list, operators amount, unique_operators_amount): Tuple containing operators and their occurence count, list containing operators and integer number of operators, integer number of unique operators.
     """
     operators_dict = {}
     operators_list = []
@@ -28,7 +30,9 @@ def operators_count(code):
             else:
                 operators_dict[char] +=1
     operators_amount = len(operators_list)
-    return(operators_dict, operators_list, operators_amount)
+    unique_operators_amount = [x for x in operators_dict.keys()]
+    unique_operators_amount = len(unique_operators_amount)
+    return(operators_dict, operators_list, operators_amount, unique_operators_amount)
 
 
 def operands_count(code):
@@ -36,15 +40,40 @@ def operands_count(code):
     This function extracts the operands in a code and puts them in a code by isolating the operators and returning returnig what is left
 
     paratemer:
-    code: code string
+     code: code string
 
     return:
-    operands_list: integer sum of operands.
+     (operands_amount, unique_operands_amount): integer number of operands and integer number of unique operands.
     """
+    operands_dict = {}
     operands_list = []
 
     for char in code.split():
         if char not in operators and char not in non_operators:
             operands_list.append(char)
+            if char not in operands_dict.keys():
+                operands_dict[char] = 1
+            else:
+                operands_dict[char] +=1
     operands_amount = len(operands_list)
-    return operands_amount
+    unique_operands_amount = [x for x in operands_dict.keys()]
+    unique_operands_amount = len(unique_operands_amount)
+    return (operands_amount, unique_operands_amount)
+
+
+def code_length_column(unique_operators_amount, unique_operands_amount):
+    """
+    This function takes number of unique operators, number of unique operands and returns the codes estimate length and volumn.
+
+    parameters:
+     code: code string
+
+    return:
+     (length, volumn) float values of code estimated length and volumn
+    """
+
+    n1, n2 = unique_operators_amount, unique_operands_amount
+    n = n1 + n2
+    length = (n1 * log2(n1) + n2 * log2(n2))
+    volumn = length * log2(n)
+    return(length, volumn)
